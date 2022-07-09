bba60c06871367f3c420afe4888f9022e7915522
Muhammad Ibrahim <hama.s.ibrahim9@gmail.com>
2022-07-09 23:20:17 +0300

    updating gitignore

--- lib/src/service/recipe_service.dart
+++ lib/src/service/recipe_service.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart';
 
-const String apiKey = '<API Key>';
-const String apiUrl = '<Url>';
+const String apiKey = '<6ca0026f6be74680a38b3c9e766a0180>';
+const String apiUrl = 'https://api.spoonacular.com/recipes/complexSearch';
 
 //!: SpoonacularAPI
 class RecipeService {
