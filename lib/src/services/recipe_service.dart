6e4a8ca4e5c26edb757fd8e1b8825161b13abfaa
Muhammad Ibrahim <hama.s.ibrahim9@gmail.com>
2022-07-16 21:48:27 +0300

    recipe_service file

---
+++ lib/src/services/recipe_service.dart
@@ -0,0 +1,32 @@
+import 'package:http/http.dart';
+import 'package:food_recipe_final/creds.dart';
+
+// const String apiKey = <Your API key>;
+const String apiUrl = 'https://api.spoonacular.com/recipes/complexSearch';
+
+//!: SpoonacularAPI
+class RecipeService {
+  Future getData(String url) async {
+    print('Calling url: $url');
+
+    // Response and get() are from the HTTP package.
+    final response = await get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      return Future.value(response.body);
+    } else {
+      print("STATUS CODE: $response.statusCode");
+      return Future.value();
+    }
+  }
+
+  Future<dynamic> getRecipes(String query, int number) async {
+    final recipeData = await getData(
+        '$apiUrl?query=$query&fillIngredients=true&apiKey=$apiKey&addRecipeNutrition=true&addRecipeInformation=true&instructionsRequired=true&number=$number');
+    if (recipeData != null) {
+      return Future.value(recipeData);
+    } else {
+      return Future.value();
+    }
+  }
+}
