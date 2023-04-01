f365c27df26a78aa5cda62a3484d9a86265c933f
Muhammad Ibrahim <hama.s.ibrahim9@gmail.com>
2023-04-01 03:02:20 +0300

    finish

--- android/app/src/androidTest/java/com/example/food_recipe_final/MainActivityTest.java
+++ android/app/src/androidTest/java/com/example/food_recipe_final/MainActivityTest.java
@@ -1,12 +1,11 @@
-// Change this line to YOUR package name:
-package com.food_recipe_final.integration_testing_prep;
+package com.example.food_recipe_final;
 
 import androidx.test.rule.ActivityTestRule;
 import dev.flutter.plugins.integration_test.FlutterTestRunner;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 // Import MainActivity from YOUR package
-import com.food_recipe_final.integration_testing_prep.MainActivity;
+// import com.food_recipe_final.integration_testing_prep.MainActivity;
 
 @RunWith(FlutterTestRunner.class)
 public class MainActivityTest {
